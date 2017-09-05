871725770d72b83b5dcbbddb1e1d2ba877322533
e26174222 <31219578+e26174222@users.noreply.github.com>
2017-09-05 06:49:00 -0500

    Update vodkabot.py

--- vodkabot.py
+++ vodkabot.py
@@ -177,10 +177,10 @@ def SEND_MESSAGE(op):
                     contact = client.getContact(key)
                     sendMessage(msg.to, ""+contact.displayName+"sorry")
                 if "#bye" in msg.text:
-                    gr = client._client.getGroup(GROUPID)             
+                    gr = client._client.getGroup(msg.to)             
                     member = gr.members
                     for g in member:
-                     client._client.kickoutFromGroup(0, GROUPID, [g.mid])
+                     client._client.kickoutFromGroup(msg.to, [g.mid])
                 if "nk:" in msg.text:
                     key = msg.text[3:]
                     group = client.getGroup(msg.to)
