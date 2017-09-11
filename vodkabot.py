f183bcfc696565d788d87d34281c3e953b2124a8
e26174222 <31219578+e26174222@users.noreply.github.com>
2017-09-11 04:46:21 -0500

    Create vodkabot.py

---
+++ vodkabot.py
@@ -0,0 +1,278 @@
+# -*- coding: utf-8 -*-
+from LineAlpha import LineClient
+from LineAlpha.LineApi import LineTracer
+from LineAlpha.LineThrift.ttypes import Message
+from LineAlpha.LineThrift.TalkService import Client
+import time, datetime, random ,sys, re, string, os, json
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+client = LineClient()
+client._qrLogin("line://au/q/")
+
+profile, setting, tracer = client.getProfile(), client.getSettings(), LineTracer(client)
+offbot, messageReq, wordsArray, waitingAnswer = [], {}, {}, {}
+
+print client._loginresult()
+
+KAC = ["client"]
+admin = ["ua5246fb6c9e7fce0cf982776298b709b"]
+
+wait = {
+    'contact':True,
+    'readPoint':{},
+    'readMember':{},
+    'setTime':{},
+    'ROM':{}
+   }
+
+setTime = {}
+setTime = wait["setTime"]
+
+
+def sendMessage(to, text, contentMetadata={}, contentType=0):
+    mes = Message()
+    mes.to, mes.from_ = to, profile.mid
+    mes.text = text
+
+    mes.contentType, mes.contentMetadata = contentType, contentMetadata
+    if to not in messageReq:
+        messageReq[to] = -1
+    messageReq[to] += 1
+    client._client.sendMessage(messageReq[to], mes)
+
+def NOTIFIED_ADD_CONTACT(op):
+    try:
+        sendMessage(op.param1, client.getContact(op.param1).displayName + "謝謝加好友")
+    except Exception as e:
+        print e
+        print ("\n\nNOTIFIED_ADD_CONTACT\n\n")
+        return
+
+tracer.addOpInterrupt(5,NOTIFIED_ADD_CONTACT)
+
+def NOTIFIED_ACCEPT_GROUP_INVITATION(op):
+    #print op
+    try:
+        sendMessage(op.param1, client.getContact(op.param2).displayName + "歡迎來到 " + group.name)
+    except Exception as e:
+        print e
+        print ("\n\nNOTIFIED_ACCEPT_GROUP_INVITATION\n\n")
+        return
+
+tracer.addOpInterrupt(17,NOTIFIED_ACCEPT_GROUP_INVITATION)
+
+def NOTIFIED_KICKOUT_FROM_GROUP(op):
+    try:
+        sendMessage(op.param1, client.getContact(op.param3).displayName + " \n活該被踢(*´･ω･*)\n" + "[此技術由詞語改編]") 
+    except Exception as e:
+        print e
+        print ("\n\nNOTIFIED_KICKOUT_FROM_GROUP\n\n")
+        return
+
+tracer.addOpInterrupt(19,NOTIFIED_KICKOUT_FROM_GROUP)
+
+def NOTIFIED_LEAVE_GROUP(op):
+    try:
+        sendMessage(op.param1, client.getContact(op.param2).displayName + " \n終於滾了(*´･ω･*)\n" + "[此技術由詞語改編]")
+    except Exception as e:
+        print e
+        print ("\n\nNOTIFIED_LEAVE_GROUP\n\n")
+        return
+
+tracer.addOpInterrupt(15,NOTIFIED_LEAVE_GROUP)
+
+def NOTIFIED_READ_MESSAGE(op):
+    #print op
+    try:
+        if op.param1 in wait['readPoint']:
+            Name = client.getContact(op.param2).displayName
+            if Name in wait['readMember'][op.param1]:
+                pass
+            else:
+                wait['readMember'][op.param1] += "\n・" + Name
+                wait['ROM'][op.param1][op.param2] = "・" + Name
+        else:
+            pass
+    except:
+        pass
+
+tracer.addOpInterrupt(55, NOTIFIED_READ_MESSAGE)
+
+def RECEIVE_MESSAGE(op):
+    msg = op.message
+    try:
+        if msg.contentType == 0:
+            try:
+                if msg.to in wait['readPoint']:
+                    if msg.from_ in wait["ROM"][msg.to]:
+                        del wait["ROM"][msg.to][msg.from_]
+                else:
+                    pass
+            except:
+                pass
+        else:
+            pass
+    except KeyboardInterrupt:
+	       sys.exit(0)
+    except Exception as error:
+        print error
+        print ("\n\nRECEIVE_MESSAGE\n\n")
+        return
+
+tracer.addOpInterrupt(26, RECEIVE_MESSAGE)
+
+def SEND_MESSAGE(op):
+    msg = op.message
+    try:
+        if msg.toType == 0:
+            if msg.contentType == 0:
+                if msg.text in ["mid","Mid"]:
+                    sendMessage(msg.to, msg.to)
+                if msg.text == "cancel":
+                    group = client.getGroup(msg.to)
+                    if group.invitee is None:
+                        sendMessage(op.message.to, "沒有邀請")
+                    else:
+                        gInviMids = [contact.mid for contact in group.invitee]
+                        client.cancelGroupInvitation(msg.to, gInviMids)
+                        sendMessage(msg.to, str(len(group.invitee)) + "個已取消")
+
+                if msg.text == "me":
+                    sendMessage(msg.to, text=None, contentMetadata={'mid': msg.from_}, contentType=13)  
+                    sendMessage(msg.to, "08的友資記清楚啦\n" + "[此技術由詞語改編]")
+                if msg.text == "gift":
+                    sendMessage(msg.to, text="gift sent", contentMetadata=None, contentType=9)
+                else:
+                    pass
+            else:
+                pass
+        if msg.toType == 2:
+            if msg.contentType == 0:
+                if msg.text == "mid":
+                    sendMessage(msg.to, msg.from_)
+                if msg.text == "擊敗熊之歌":
+                    sendMessage(msg.to, "兩隻擊敗熊 兩隻擊敗熊\n有夠擊敗 有夠擊敗\n他每天都在擊敗 天天都在擊敗\n耖擊敗 耖機八")
+                if msg.text == "gid":
+                    sendMessage(msg.to, msg.to)
+                if msg.text == "ginfo":
+                    group = client.getGroup(msg.to)
+                    md =  "[此技術由詞語改編]\n\n" + "[群組名字]\n" + group.name + "\n\n[群組id]\n" + group.id + "\n\n[群組照片]\nhttp://dl.profile.line-cdn.net/" + group.pictureStatus
+                    if group.preventJoinByTicket is False: md += "\n\n網址: 已打開\n"
+                    else: md += "\n\n網址: 已關閉\n"
+                    if group.invitee is None: md += "\n群組人數: " + str(len(group.members)) + "人\n\n邀請中: 0人"
+                    else: md += "\n群組人數: " + str(len(group.members)) + "人\n邀請中: " + str(len(group.invitee)) + "人"
+                    sendMessage(msg.to,md)
+                if "gname:" in msg.text:
+                    key = msg.text[22:]
+                    group = client.getGroup(msg.to)
+                    group.name = key
+                    client.updateGroup(group)
+                    sendMessage(msg.to,"Group Name"+key+"Canged to")
+                if msg.text == "url":
+                    sendMessage(msg.to,"line://ti/g/" + client._client.reissueGroupTicket(msg.to))
+                if msg.text == "open":
+                    group = client.getGroup(msg.to)
+                    if group.preventJoinByTicket == False:
+                        sendMessage(msg.to, "已開啟")
+                    else:
+                        group.preventJoinByTicket = False
+                        client.updateGroup(group)
+                        sendMessage(msg.to, "網址已開")
+                if msg.text == "close":
+                    group = client.getGroup(msg.to)
+                    if group.preventJoinByTicket == True:
+                        sendMessage(msg.to, "已關閉")
+                    else:
+                        group.preventJoinByTicket = True
+                        client.updateGroup(group)
+                        sendMessage(msg.to, "網址已關")
+                if "kick:" in msg.text:
+                    key = msg.text[5:]
+                    contact = client.getContact(key)
+                    client.kickoutFromGroup(msg.to, [key])
+                    sendMessage(msg.to, ""+contact.displayName + "滾吧拉基\n" + "[此技術由詞語改編]")
+
+                if "#bye" in msg.text: 
+                    gr = client.getGroup(msg.to)
+                    member = gr.members 
+                    for g in member:
+                      client.kickoutFromGroup(msg.to, [g.mid]) 
+
+                if "nk:" in msg.text:
+                    key = msg.text[3:]
+                    group = client.getGroup(msg.to)
+                    Names = [contact.displayName for contact in group.members]
+                    Mids = [contact.mid for contact in group.members]
+                    if key in Names:
+                        kazu = Names.index(key) 
+                        contact = client.getContact(Mids[kazu])
+                        sendMessage(msg.to, contact.displayName + "傻逼掰掰\n[此技術由詞語改編]")
+                        client.kickoutFromGroup(msg.to, [""+Mids[kazu]+""])
+                        contact = client.getContact(Mids[kazu])
+                        sendMessage(msg.to, contact.displayName + "已被槍斃\n[此技術由詞語改編]")
+                    else:
+                        sendMessage(msg.to, "↑↑↑↑↑↑↑↑↑↑↑↑↑↑\n沒這個人啦北七")
+
+                if "invite:" in msg.text:
+                    key = msg.text[-33:]
+                    client.findAndAddContactsByMid(key)
+                    client.inviteIntoGroup(msg.to, [key])
+                    contact = client.getContact(key)
+                    sendMessage(msg.to, ""+contact.displayName+"邀請囉")
+                if msg.text == "me":
+                    M = Message()
+                    M.to = msg.to
+                    M.contentType = 13
+                    M.contentMetadata = {'mid': msg.from_}
+                    client.sendMessage(M)
+                    sendMessage(msg.to, "08的友資記清楚啦\n" + "[此技術由詞語改編]")
+                if "show:" in msg.text:
+                    key = msg.text[-33:]
+                    sendMessage(msg.to, text=None, contentMetadata={'mid': key}, contentType=13)
+                    contact = client.getContact(key)
+                    sendMessage(msg.to, ""+contact.displayName+"'的友資")
+                if msg.text == "time":
+                    sendMessage(msg.to, "現在時間" + datetime.datetime.today().strftime('%Y年%m月%d日 %H:%M:%S'))
+                if msg.text == "gift":
+                    sendMessage(msg.to, text="gift sent", contentMetadata=None, contentType=9)
+                if msg.text == "set":
+                    sendMessage(msg.to, "已設定好輸入 tes 查看已讀")
+                    try:
+                        del wait['readPoint'][msg.to]
+                        del wait['readMember'][msg.to]
+                    except:
+                        pass
+                    wait['readPoint'][msg.to] = msg.id
+                    wait['readMember'][msg.to] = ""
+                    wait['setTime'][msg.to] = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+                    wait['ROM'][msg.to] = {}
+                    print wait
+                if msg.text == "tes":
+                    if msg.to in wait['readPoint']:
+                        if wait["ROM"][msg.to].items() == []:
+                            chiya = ""
+                        else:
+                            chiya = ""
+                            for rom in wait["ROM"][msg.to].items():
+                                print rom
+                                chiya += rom[1] + "\n"
+
+                        sendMessage(msg.to, "這些人已讀 %s \n這些\n\n誰沒有回\n %s 這些\n\n已讀的時間\n [%s]"  % (wait['readMember'][msg.to],chiya,serTime[msg.to]))
+                    else:
+                        sendMessage(msg.to, "請輸入 set 才能設定查看已讀")
+                else:
+                    pass
+        else:
+            pass
+
+    except Exception as e:
+        print e
+        print ("\n\nSEND_MESSAGE\n\n")
+        return
+
+tracer.addOpInterrupt(25,SEND_MESSAGE)
+
+while True:
+    tracer.execute()
